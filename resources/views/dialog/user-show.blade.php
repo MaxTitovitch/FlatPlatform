9879b81ea3cf37aa28247981a740673403d95a4f
IlyaGitHub <ilya-levoshko@mail.ru>
2020-09-14 21:46:07 +0300

    Service CRUD pages were created

--- resources/views/dialog/user-show.blade.php
+++ resources/views/dialog/user-show.blade.php
@@ -8,8 +8,34 @@
 
 @section('right-bar')
     <div class="col-md-2">
-        <a href="{{ route('dialog-support') }}" class="dialog-support mt-3">
-            Связаться с техподдержкой
-        </a>
+
+        <div class="w-100 border-left border-primary h-100">
+            <form action="" method="post" class="mt-md-2 color-bg-dark-blue w-100 text-center">
+                @csrf
+                <input type="hidden" name="_method" value="PATCH">
+                <button type="submit" class="color-bg-dark-blue text-white border-0 py-md-2">ПРИНЯТЬ</button>
+            </form>
+            <form action="" method="post" class="mt-md-2 color-bg-dark-blue w-100 text-center">
+                @csrf
+                <input type="hidden" name="_method" value="PATCH">
+                <button type="submit" class="color-bg-dark-blue text-white border-0 py-md-2">ОТКЛОНИТЬ</button>
+            </form>
+            <form action="" method="post" class="mt-md-2 color-bg-dark-blue w-100 text-center">
+                @csrf
+                <input type="hidden" name="_method" value="PATCH">
+                <button type="submit" class="color-bg-dark-blue text-white border-0 py-md-2">ОТОЗВАТЬ</button>
+            </form>
+            <form action="" method="post" class="mt-md-2 color-bg-dark-blue w-100 text-center">
+                @csrf
+                <input type="hidden" name="_method" value="PATCH">
+                <button type="submit" class="color-bg-dark-blue text-white border-0 py-md-2">УТВЕРДИТЬ</button>
+            </form>
+            <form action="" method="post" class="mt-md-2 color-bg-dark-blue w-100 text-center">
+                @csrf
+                <input type="hidden" name="_method" value="PATCH">
+                <button type="submit" class="color-bg-dark-blue text-white border-0 py-md-2">ПРОЕКТ ВЫПОЛНЕН</button>
+            </form>
+        </div>
+
     </div>
 @endsection
