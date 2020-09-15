5583a10dedf824d066b22a2840ddf9fa06828f3d
IlyaGitHub <ilya-levoshko@mail.ru>
2020-09-15 04:05:33 +0300

    Dialog fisex

--- resources/views/dialog/part/show.blade.php
+++ resources/views/dialog/part/show.blade.php
@@ -48,13 +48,13 @@
                     @foreach($messages as $message)
                         @if(Auth::id() !== $message->user_id)
                             <div class="first-user-message mt-md-2  text-white">
-                                <span class="bg-primary px-md-2 border rounded">
+                                <span class="bg-primary px-md-2 rounded">
                                     {{ $message->message }} <span class="message-time">{{ substr($message->created_at, 11, 5) }}</span>
                                 </span>
                             </div>
                         @else
                             <div class="second-user-message mt-md-2  text-white" style="margin-right: 15px;">
-                                <span class="color-bg-dark-blue px-md-2 border rounded">
+                                <span class="color-bg-dark-blue px-md-2 rounded">
                                     {{ $message->message }} <span class="message-time">{{ substr($message->created_at, 11, 5) }}</span>
                                 </span>
                             </div>
@@ -68,13 +68,13 @@
                     @foreach($newMessages as $message)
                         @if(Auth::id() !== $message->user_id)
                             <div class="first-user-message mt-md-2  text-white">
-                                <span class="bg-primary px-md-2 border rounded">
+                                <span class="bg-primary px-md-2 rounded">
                                     {{ $message->message }} <span class="message-time">{{ substr($message->created_at, 11, 5) }}</span>
                                 </span>
                             </div>
                         @else
                             <div class="second-user-message mt-md-2  text-white" style="margin-right: 15px;">
-                                <span class="color-bg-dark-blue px-md-2 border rounded">
+                                <span class="color-bg-dark-blue px-md-2 rounded">
                                     {{ $message->message }} <span class="message-time">{{ substr($message->created_at, 11, 5) }}</span>
                                 </span>
                             </div>
@@ -90,7 +90,7 @@
                 <div class="">
                     <input type="file" class="display-none">
                     <svg width="48" height="47" viewBox="0 0 48 47" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <g clip-path="url(#clip0)">
+                        <g >
                             <path
                                 d="M22.6272 13.2911L33.8084 24.4723C35.9571 26.621 35.9571 30.1017 33.8084 32.2504C31.6596 34.3992 28.1789 34.3992 26.0302 32.2504L13.8768 20.097C12.5351 18.7553 12.5351 16.5774 13.8768 15.2357C15.2185 13.894 17.3964 13.894 18.7381 15.2357L28.947 25.4445C29.4818 25.9793 29.4818 26.8543 28.947 27.3891C28.4123 27.9238 27.5372 27.9238 27.0025 27.3891L17.7659 18.1525L16.3075 19.6109L25.5441 28.8475C26.8858 30.1892 29.0637 30.1892 30.4054 28.8475C31.7471 27.5058 31.7471 25.3279 30.4054 23.9861L20.1966 13.7773C18.0478 11.6286 14.5671 11.6286 12.4184 13.7773C10.2697 15.926 10.2697 19.4067 12.4184 21.5555L24.5718 33.7089C27.5275 36.6646 32.3111 36.6646 35.2668 33.7089C38.2225 30.7531 38.2225 25.9696 35.2668 23.0139L24.0856 11.8327L22.6272 13.2911Z"
                                 fill="#183E62"/>
@@ -103,9 +103,7 @@
                         </defs>
                     </svg>
                 </div>
-                <div class=" mr-md-2 my-md-auto color-bg-dark-blue text-white px-md-2 py-md-1" style="border-radius: 25px">
-                    Отправить
-                </div>
+                <input type="submit" class=" mr-md-2 my-md-auto border-0 color-bg-dark-blue text-white px-md-2 py-md-1" style="border-radius: 25px">
             </div>
         </form>
     </div>
